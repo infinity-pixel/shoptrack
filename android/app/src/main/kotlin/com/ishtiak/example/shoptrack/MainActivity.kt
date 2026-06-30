2278e288187ac112bc921c28703c3466cfdaa390
Ishtiak Ahmed <ishtiakahmed0077@gmail.com>
2026-07-01 01:40:01 +0600

    Initial Flutter project

---
+++ android/app/src/main/kotlin/com/ishtiak/example/shoptrack/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.ishtiak.example.shoptrack
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'features/home/presentation/home_page.dart';
+
+class ShopTrackApp extends StatelessWidget {
+  const ShopTrackApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ShopTrack',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.dark,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
